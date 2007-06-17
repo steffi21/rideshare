052f18a6485c90fa82eb98445fb3caabb9f123f2
mike.john.james <mike.john.james@58d43213-ee32-0410-b9bd-471ee5dae657>
2007-06-17 10:33:03 +0000

    Some UI Changes

--- trunk/src/rideShareApp/controls/map.ascx
+++ trunk/src/rideShareApp/controls/map.ascx
@@ -1,5 +1,5 @@
 <%@ Control Language="C#" AutoEventWireup="true" CodeBehind="map.ascx.cs" Inherits="rideShareApp.WebUserControl1" %>
 
 <div id="mapContainer">
-height: 
+
 </div>
--- trunk/src/rideShareApp/javascript/map.js
+++ trunk/src/rideShareApp/javascript/map.js
@@ -8,9 +8,9 @@ YAHOO.rideShare.map = {
     // Create a lat/lon object
     var myPoint = "UK";
     // Create a map object
-    var map = new YMap(document.getElementById('mapContainer'));
+    map = new YMap(document.getElementById('mapContainer'));
     // Display the map centered on a latitude and longitude
-    map.drawZoomAndCenter(myPoint, 14); //default to UK View
+    map.drawZoomAndCenter(myPoint, 13); //default to UK View
 
     // Add map type control
     map.addTypeControl();
@@ -24,11 +24,31 @@ YAHOO.rideShare.map = {
     map.geoCodeAddress("l9 8df, england")
 
     // Set map type to either of: YAHOO_MAP_SAT YAHOO_MAP_HYB YAHOO_MAP_REG
-    map.setMapType(YAHOO_MAP_SAT);
+    map.setMapType(YAHOO_MAP_REG);
 
     //Get valid map types, returns array [YAHOO_MAP_REG, YAHOO_MAP_SAT, YAHOO_MAP_HYB]
     var myMapTypes = map.getMapTypes();
+    },
+    
+    updateMap:function(e){
+      var myNewPoint = this.value;
+      map.drawZoomAndCenter(myNewPoint, 4); //default to UK View
+      var myLatLan = document.getElementById("hiddenLatLan");
+      myGeoPoint = map.getCenterLatLon();
+      myLatLan.value = myGeoPoint.Lat + ',' + myGeoPoint.Lon;
+      myMarker = new YMarker(myGeoPoint, "homeMarker");
+      myMarker.addAutoExpand = "Location: "+ document.getElementById("txtPostCode").value;
+      
+      map.addMarker(myNewPoint);
+    },
+    
+    loadAttendees:function(){
+        var eventid = document.getElementById('selectEvent');
+        map.YGeoRSS('http://rideShare.geeksbox.co.uk/datafeed.ashx/xml/mapdata/'+ eventid);
     }
 };
 
 YAHOO.util.Event.onContentReady('mapContainer', YAHOO.rideShare.map.init);
+YAHOO.util.Event.addListener('txtPostCode', 'change', YAHOO.rideShare.map.updateMap);
+YAHOO.util.Event.addListener('hiddenEventPostCode', 'change', YAHOO.rideShare.map.updateMap);
+//YAHOO.util.Event.addListener('hiddenEventPostCode', 'change', YAHOO.rideShare.map.loadAttendees);
